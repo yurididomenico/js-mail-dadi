ea3ff723a57da772d825de90ef7ff23d068b001c
yurididomenico <yurididomenico93@gmail.com>
2022-10-24 16:29:53 +0200

    Aggiungo HTML tramite JS

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,52 +5,55 @@
 Chiedi all’utente la sua email,
 controlla che sia già presente nella lista di email per controllare se può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
-
-~ Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-scrivere quante partite ha vinto il giocatore
-scrivere quante partite ha vinto il computer
 */
 
 console.log("Ciao!");
 
 let userMail = ["test", "test", "test", "ciccio@gmail.it", "ydd@gmail.it", "daryan@gmail.it"]
 let outputDiv = document.getElementById('outputDiv');
-
-
+let outputGame = document.getElementById('gameDice');
+let linea = document.getElementById('linea');
 
 function checkList()
 {
-    // Prendo il valore inserito dall'utente
+    // Prendo il valore inserito dall'utente & Creo una variabile flag (false)
     let inputUserMail = document.getElementById('inputUserMail').value;
-    // Creo una variabile flag
     let check = false;
 
 
     // Controllo il vettore con un ciclo for
     for(let i=0; i<userMail.length; i++)
     {
-        // outputDiv.innerHTML += `<p>Utente: ${userMail[i]}</p>`;
-        if(inputUserMail == userMail[i])
-        {
-            check = true;
-        }
+        if(inputUserMail == userMail[i]) check = true;        
     }
     
 
-    // Aggiungo una condizione èer l'output
+    // Aggiungo una condizione per l'output
     if(check == true)
     {
-        outputDiv.innerHTML += `<p>L'utente è presente nella lista!</p>`;
-    }
+        outputDiv.innerHTML += `<p class="bg-success text-white p-3 rounded">L'utente è presente nella lista!</p>`;
+        linea.classList.remove('d-none');
+        outputGame.classList.remove('d-none');
+    }   
     else
     {
-        outputDiv.innerHTML += `<p>L'utente è inesistente!</p>`;
+        outputDiv.innerHTML += `<p class="bg-danger text-white p-3 rounded">L'utente è inesistente!</p>`;
+        linea.classList.remove('d-none');
+        userMail.push(inputUserMail);
+        outputGame.classList.remove('d-none');
+        outputGame.innerHTML += `<p>L'email è stata aggiunta al nostro database! <i class="fa-solid fa-thumbs-up"></i><hr></p>`;
     }
+    
 }
 
-
+//----------------------------------------------------//
+/*
+~ Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+scrivere quante partite ha vinto il giocatore
+scrivere quante partite ha vinto il computer
+*/
 
 
 
