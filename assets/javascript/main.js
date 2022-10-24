88e65adcc52ef26b51a22f25501dc0a58e2f0464
yurididomenico <yurididomenico93@gmail.com>
2022-10-24 14:49:40 +0200

    Layout form & check input

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,28 @@
 // JAVASCRIPT //
 
+/*
+~ Mail
+Chiedi all’utente la sua email,
+controlla che sia già presente nella lista di email per controllare se può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+~ Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+scrivere quante partite ha vinto il giocatore
+scrivere quante partite ha vinto il computer
+*/
+
 console.log("Ciao!");
 
+let userMail = ["ciccio@gmail.it", "ydd@gmail.it", "daryan@gmail.it", "coco@gmail.it", "a@gmail.it", ]
+let inputUserMail = document.getElementById('inputUserMail');
 
-
+function checkList()
+{
+    console.log(inputUserMail.value);
+    
+}
 
 
 
